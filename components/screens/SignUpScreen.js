ff978cb12f6729b8e6ae74b9105992d28120f710
Dainis DM. Moisejenko <damo@pc-tec.local>
2020-08-31 14:25:58 +0200

    Added a stack navigator and 3 first screens

---
+++ components/screens/SignUpScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const SignUpScreen = () => {
+  return (
+    <View>
+      <Text>SignUp Screen</Text>
+    </View>
+  );
+};
+
+export default SignUpScreen;
---
+++ components/screens/WelcomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const WelcomeScreen = () => {
+  return (
+    <View>
+      <Text>Welcome Screen</Text>
+    </View>
+  );
+};
+
+export default WelcomeScreen;
