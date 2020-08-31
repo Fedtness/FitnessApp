d9048036664b1acc83394c32aa03eed31c841284
Dainis DM. Moisejenko <damo@pc-tec.local>
2020-08-31 14:51:15 +0200

    Started working on Welcome screen /ImageBackground

--- components/screens/WelcomeScreen.js
+++ components/screens/WelcomeScreen.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {ImageBackground, StyleSheet, Text, View} from 'react-native';
 
 const WelcomeScreen = () => {
   return (
-    <View>
-      <Text>Welcome Screen</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        source={require('../../assets/WelcomeBackground.jpg')}
+        style={styles.backgroundImage}>
+        <View style={styles.mainContainer}>
+          <Text style={{fontSize: 36, fontWeight: 'bold', color: 'blue'}}>
+            Welcome screen
+          </Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  mainContainer: {
+    opacity: 1,
+    fontSize: 36,
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    opacity: 0.7,
+  },
+});
 
 export default WelcomeScreen;
