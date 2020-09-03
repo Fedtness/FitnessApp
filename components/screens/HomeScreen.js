7857ef5df65251120a0f17275347d4ae282b6992
Dainis DM. Moisejenko <damo@pc-tec.local>
2020-09-03 10:28:13 +0200

    Made a home screen navigation  & added new screens

--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import HomeBottomTabs from '../routes/homeBottomTabs';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      <HomeBottomTabs />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#002930',
+  },
+});
 
 export default HomeScreen;
